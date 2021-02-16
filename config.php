cb7bff4c034e07a79dc5a3bd934139232aed6d76
Olzhassss <Olzhassss@users.noreply.github.com>
2021-02-16 13:01:07 +0600

    Deleted outdated files, restructured config.php

--- config.php
+++ config.php
@@ -9,20 +9,16 @@ else
 $url.= $_SERVER['HTTP_HOST'];   
 
 // The domain URL address of the website. Used for 'href' and 'src' attributes of html tags
-$root_url = $url;
-
-// Full URL address of the page
-$url.= $_SERVER['REQUEST_URI'];    
+$root_url = $url;   
 
 // Root ldp on the server. Used for the 'include_once' functions of PHP
-// Note: LDP stands for local directory path
-if (!isset($root_ldp)) {
-	$root_ldp = $_SERVER['DOCUMENT_ROOT'];
-}
-// This configs file LDP
-//$config = $root_ldp . "/config.php";
+// Note: LDP stands for Local Directory Path
+$root_ldp = $_SERVER['DOCUMENT_ROOT'];
 
-// The connection configs file LDP
+// Full URL address of the page
+$url.= $_SERVER['REQUEST_URI']; 
+
+// The connection config file LDP
 $connection_config = $root_ldp . '/connection_config.php';
 
 // The main webpage URL
@@ -37,13 +33,27 @@ $css = $root_url . '/stylesheets' . '/';
 // Location of the images (part of URL)
 $imgs = $root_url . '/images' . '/';
 
-// Location of general (public) pages as both URL and LDP
+// Location of general (public) pages (URL and LDP)
 $general_url = $root_url . '/general' . '/';
 $general_ldp = $root_ldp . '/general' . '/';
 
 // Location of the pages for authorized users (URL and LDP)
 $administration_url = $root_url . '/administration' . '/';
 $administration_ldp = $root_ldp . '/administration' . '/';
+
+// Location of the pages concerning applications (URL and LDP)
+$applications_url = $administration_url . 'applications' . '/';
+$applications_ldp = $administration_ldp . 'applications' . '/';
+
+// Location of the pages concerning members (URL and LDP)
+$members_url = $administration_url . 'members' . '/';
+$members_ldp = $administration_ldp . 'members' . '/';
+
+// Location of the pages concerning classes (URL)
+$classes_url = $administration_url . 'classes' . '/';
+
+// Location of the pages concerning schedule (URL)
+$schedule_url = $administration_url . 'schedule' . '/';
 
 // Location of the files needed for any kind of processing (without html) (URL and LDP)
 $processing_url = $root_url . '/processing' . '/';
@@ -53,6 +63,13 @@ $processing_ldp = $root_ldp . '/processing' . '/';
 $miscellaneous_ldp = $root_ldp . '/miscellaneous' . '/';
 $miscellaneous_url = $root_url . '/miscellaneous' . '/';
 
+// The LDPs of headers, footer, head tag, '<-Back' link
+$header_ldp = $root_ldp .'/header-footer/header.php';
+$headerAdmin_ldp = $root_ldp .'/header-footer/headerAdmin.php';
+$footer_ldp = $root_ldp .'/header-footer/footer.php';
+$head_ldp = $root_ldp .'/head.php';
+$backLink_ldp = $miscellaneous_ldp . 'backLink.php';
+
 // The URLs of the files at 'index.php' header navigation bar ('header.php')
 // (main pre-application page, authorization page)
 $appMain_url = $general_url . 'app_main.php';
@@ -61,7 +78,7 @@ $authorizationPage_url = $general_url . 'authorization_page.php';
 // The group or private lesson choice (student pre-application) webpage URL
 $appStd_url = $general_url . 'app_s0.php';
 
-// URLs to the application webpages
+// The URLs to the application webpages
 $appStdPrivate_url = $general_url .'app_s_pr.php';
 $appStdGroup_url = $general_url .'app_s_gr.php';
 $appTch_url = $general_url .'app_t.php';
@@ -76,34 +93,35 @@ $adminIndex_url = $administration_url .'admin_index.php';
 // (log out page)
 $logout_url = $processing_url . 'logout.php';
 
-// URLs of the injectable php files
-$appInject_url = $administration_url .'applications_injection.php';
-$schInject_url = $administration_url .'schedule_injection.php';
-$clsInject_url = $administration_url .'classes_injection.php';
+// The URLs of the injectable php files
 $edtInject_url = $administration_url .'frontEdit_injection.php';
-$memInject_url = $administration_url . 'members_injection.php';
-$appCvInject_url = $administration_url .'appBrowse_injection.php';
-$memCvInject_url = $administration_url .'memBrowse_injection.php';
-$appTables_url = $administration_url . 'appTables_injection.php';
-$memTables_url = $administration_url . 'memTables_injection.php';
 
-$memEditInject_url = $administration_url . 'memEdit_injection.php';
+$appInject_url = $applications_url .'applications_injection.php';
+$appTables_url = $applications_url . 'appTables_injection.php';
+$appCvInject_url = $applications_url .'appBrowse_injection.php';
 
-$clsInfoInject_url = $administration_url . 'clsBrowse_injection.php';
-$clsEditInject_url = $administration_url . 'clsEdit_injection.php';
+$memCvInject_url = $members_url .'memBrowse_injection.php';
+$memInject_url = $members_url . 'members_injection.php';
+$memTables_url = $members_url . 'memTables_injection.php';
+$memEditInject_url = $members_url . 'memEdit_injection.php';
 
-$schEditInject_url = $administration_url . 'schEdit_injection.php';
-$schEditOver_url = $administration_url . 'scheduleOverlay_injection.php';
-$schStdEdtTbInject_url = $administration_url . 'schEditTable_injection.php';
+$clsInject_url = $classes_url .'classes_injection.php';
+$clsInfoInject_url = $classes_url . 'clsBrowse_injection.php';
+$clsEditInject_url = $classes_url . 'clsEdit_injection.php';
 
-// The LDPs of headers, footer, head tag, '<-Back' link
-$header_ldp = $root_ldp .'/header-footer/header.php';
-$headerAdmin_ldp = $root_ldp .'/header-footer/headerAdmin.php';
-$footer_ldp = $root_ldp .'/header-footer/footer.php';
-$head_ldp = $root_ldp .'/head.php';
-$backLink_ldp = $miscellaneous_ldp . 'backLink.php';
+$schInject_url = $schedule_url .'schedule_injection.php';
+$schEditInject_url = $schedule_url . 'schEdit_injection.php';
+$schStdEdtTbInject_url = $schedule_url . 'schEditTable_injection.php';
 
-// The URLs of the files in 'processing'
+// The LDPs of the injectable php files
+$appTables_ldp = $applications_ldp . 'appTables_injection.php';
+$memTables_ldp = $members_ldp . 'memTables_injection.php';
+$memCvInject_ldp = $members_ldp .'memBrowse_injection.php';
+$memEditInject_ldp = $members_ldp . 'memEdit_injection.php';
+$memTchEdtTbInject_ldp = $members_ldp . 'memEditTablesTch_injection.php';
+$memStdEdtTbInject_ldp = $members_ldp . 'memEditTablesStd_injection.php';
+
+// The URLs of the files in 'processing' folder
 $router_url = $processing_url . 'admin_routing.php';
 $appProcessing_url = $processing_url . 'app_processing.php';
 $recordProcessing_url = $processing_url . 'record_processing.php';
@@ -113,14 +131,6 @@ $classProcessing_url = $processing_url . 'class_processing.php';
 $scheduleProcessing_url = $processing_url . 'schedule_processing.php';
 $authorizationProcessing_url = $processing_url . 'authorization_processing.php';
 $txtUpdProcessing_url = $processing_url . 'texts_processing.php';
-
-// The LDPs of the injectable php files
-$appTables_ldp = $administration_ldp . 'appTables_injection.php';
-$memTables_ldp = $administration_ldp . 'memTables_injection.php';
-$memCvInject_ldp = $administration_ldp .'memBrowse_injection.php';
-$memEditInject_ldp = $administration_ldp . 'memEdit_injection.php';
-$memTchEdtTbInject_ldp = $administration_ldp . 'memEditTablesTch_injection.php';
-$memStdEdtTbInject_ldp = $administration_ldp . 'memEditTablesStd_injection.php';
 
 // Miscellaneous
 $spinner_src = $imgs . "spinner.gif";
@@ -152,12 +162,11 @@ switch ($url) {
 }
 
 function exception_handler($exception){
-	echo "<b> Exception: </b>" . $exception->getMessage();
+	echo "<b> Exception occured: </b>" . $exception->getMessage();
 }
 
 set_exception_handler('exception_handler');
 
 ini_set('session.cookie_httponly',1);
-//ini_set('session.cookie_secure',1);
 ini_set('session.use_strict_mode',1);
 ?>
